63f85e4f6167fa233c212489463f032196f8e39d
Andrej Koman <andrejkoman@hotmail.com>
2025-01-21 22:59:51 +0100

    feat: Login fixed

--- diplomska.client/src/app/login/page.tsx
+++ diplomska.client/src/app/login/page.tsx
@@ -1,14 +1,11 @@
-import "./App.css";
-import { LoginForm } from "./components/login-form";
+import { LoginForm } from "@/components/login-form"
 
-function App() {
+export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
     </div>
-  );
+  )
 }
-
-export default App;
